42396797c28671c03924b2bf51548c6830aaab4a
Israel Alves <ysrael.2009@hotmail.com>
2022-09-09 13:36:00 -0300

    feat: countdown

---
+++ countdown/script.js
@@ -0,0 +1,18 @@
+setInterval(function() {
+  let novaHora = new Date();
+  let dia = novaHora.getDay();
+  let hora = novaHora.getHours();
+  let minuto = novaHora.getMinutes();
+  let segundos = novaHora.getSeconds();
+
+  minuto = zero(minuto);
+  segundos = zero(segundos);
+
+  document.getElementById('count').textContent = dia + ' : ' + hora + ' : ' + minuto + ' : ' + segundos;
+}, 1000)
+
+function zero(x) {
+  if (x < 10) {
+    x = '0' + x;
+  } return x;
+}
